3d328dbf4302a9b0aa7c6c4911e6fcc15b7aac45
RomanGrabovyi <60966944+RomanGrabovyi@users.noreply.github.com>
2023-01-01 13:25:19 +0200

    add counter

---
+++ src/App.js
@@ -0,0 +1,17 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div>
+        <h2>Counter</h2>
+        <h1>0</h1>
+        <button className="plus"></button>
+        <button className="minus"></button>
+      </div>
+    </div>
+  );
+}
+
+export default App;
