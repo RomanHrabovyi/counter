fce856658e3591bddfa19b979d10fb457fc1a6e4
RomanGrabovyi <60966944+RomanGrabovyi@users.noreply.github.com>
2023-01-01 15:24:04 +0200

    mod counter

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
+import React, {useState} from 'react';
 
-import './App.css';
+import './index.scss';
 
 function App() {
+
+  const [count, setCount] = useState(0);
+
   return (
     <div className="App">
       <div>
         <h2>Counter</h2>
-        <h1>0</h1>
-        <button className="plus"></button>
-        <button className="minus"></button>
+        <h1>{count}</h1>
+        <button className="plus" onClick={() => setCount(count + 1)}>Plus +</button>
+        <button className="minus" onClick={() => setCount(count - 1)}>Minus -</button>
       </div>
     </div>
   );
